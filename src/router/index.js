baf4bda2eaccf06b42a3a282d9aeda8256e4ff23
Uliana Blinova <ulanablinova0@gmail.com>
2026-03-13 14:21:28 +0700

    Router config

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '@/store'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'Catalog',
+    component: () => import('@/views/Catalog.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/views/Register.vue')
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import('@/views/Cart.vue'),
+    meta: { requiresAuth: true }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters.isAuthenticated
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
