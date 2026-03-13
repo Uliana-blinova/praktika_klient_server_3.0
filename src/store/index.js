200a544b8d03ba1d7d22721236a1540837d43c71
Uliana Blinova <ulanablinova0@gmail.com>
2026-03-13 14:19:35 +0700

    store initial

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,35 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    token: localStorage.getItem('user_token') || '',
+    user: null,
+    cart: []
   },
   getters: {
+    isAuthenticated(state) {
+      return !!state.token
+    }
   },
   mutations: {
+    SET_TOKEN(state, token) {
+      state.token = token
+      if (token) {
+        localStorage.setItem('user_token', token)
+      } else {
+        localStorage.removeItem('user_token')
+      }
+    },
+    SET_USER(state, user) {
+      state.user = user
+    },
+    SET_CART(state, cart) {
+      state.cart = cart
+    }
   },
   actions: {
-  },
-  modules: {
+    logout({ commit }) {
+      commit('SET_TOKEN', '')
+      commit('SET_USER', null)
+    }
   }
 })
